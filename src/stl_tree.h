10d09a19e76c08cd0559a8a87f0f3128cc296dd4
chengzh <loveandloss@hotmail.com>
2020-09-18 00:44:28 +0800

    add std_tree.h: perfect class rb_tree

--- src/stl_tree.h
+++ src/stl_tree.h
@@ -1,6 +1,8 @@
 #ifndef __STL_TREE_H
 #define __STL_TREE_H
 
+#include <utility>
+
 #include "../src/stl_config.h"
 #include "../src/stl_alloc.h"
 #include "../src/stl_iterator.h"
@@ -180,6 +182,76 @@ protected:
     link_type& root() const { return (link_type&) header->parent; }
     link_type& leftmost() const { return (link_type&) header->left; }
     link_type& rightmost() const { return (link_type&) header->right; }
+
+    // 以下六个函数用来方便取得节点 x 的成员
+    static link_type& left(link_type x) { return (link_type&)(x->left); }
+    static link_type& right(link_type x) { return (link_type&)(x->right); }
+    static link_type& parent(link_type x) { return (link_type&)(x->parent); }
+    static link_type& value(link_type x) { return x->value_field; }
+    static const Key& key(link_type x) { return KeyOfValue()(value(x)); } 
+    static color_type& color(link_type x) { return (color_type&)(x->color); }
+
+    // 以下六个函数用来方便取得节点 x 的成员
+    static link_type& left(base_ptr x) { return (link_type&)(x->left); }
+    static link_type& right(base_ptr x) { return (link_type&)(x->right); }
+    static link_type& parent(base_ptr x) { return (link_type&)(x->parent); }
+    static link_type& value(base_ptr x) { return ((link_type)x)->value_field; }
+    static const Key& key(base_ptr x) { return KeyOfValue()(value(link_type(x))); } 
+    static color_type& color(base_ptr x) { return (color_type&)(link_type(x)->color); }
+
+    // 求取极大值和极小值. node class 有实现此功能, 交给它们完成即可
+    static link_type minimum(link_type x)
+    {
+        return (link_type) __rb_tree_node_base::minimum(x);
+    }
+    static link_type maximum(link_type x)
+    {
+        return (link_type) __rb_tree_node_base::maximum(x);
+    }
+
+public:
+    typedef __rb_tree_iterator<value_type, reference, pointer> iterator;
+    typedef __rb_tree_iterator<value_type, const_reference, const_pointer> const_iterator;
+
+private:
+    iterator __insert(base_ptr x, base_ptr y, const value_type& v);
+    link_type __copy(link_type x, link_type p);
+    void __erase(link_type x);
+    void clear();
+    void init()
+    {
+        header = get_node();            // 产生一个节点空间, 令 header 指向它
+        color(header) = __rb_tree_red;  // 令 header 为红色, 用来区分 header 和 root,
+                                        // 在 iterator.operator-- 之中
+        root() = 0;
+        leftmost() = header;
+        rightmost() = header;
+    }
+
+public:     // allocation/deallocation
+    rb_tree(const Compare& comp = Compare()) : node_count(0), key_compare(comp) { init(); }
+
+    ~rb_tree()
+    {
+        clear();
+        put_node(header);
+    }
+    rb_tree<Key, Value, KeyOfValue, Compare, Alloc>&
+    operator=(const rb_tree<Key, Value, KeyOfValue, Compare, Alloc>& x);
+
+public:     // accessors
+    Compare key_comp() const { return key_compare; }
+    iterator begin() { return leftmost(); }     // RB 树的起头为最左(最小)节点处
+    iterator eng() { return header; }           // RB 树的终点为 header 所指处
+    bool empty() const { return node_count == 0; }
+    size_type size() const { return node_count; }
+    size_type max_size() const { return size_type(-1); }
+
+public:     // insert/erase
+    // 将 x 插入到 RB-tree 中 (保持节点值独一无二)
+    std::pair<iterator, bool> insert_unique(const value_type& x);
+    // 将 x 插入到 RB-tree 中 (允许节点值重复)
+    iterator insert_equal(const value_type& x);
 };
 
 #endif
